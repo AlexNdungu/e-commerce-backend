f2f0556c0ba064c39e09f5c14d3c3603dcb849da
AlexNdungu <alexmeta517@gmail.com>
2024-12-14 13:09:45 +0300

    create repositories

---
+++ src/main/java/com/example/e_commerce_backend/Category/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.example.e_commerce_backend.Category;
+
+public interface CategoryRepository {
+}
---
+++ src/main/java/com/example/e_commerce_backend/Product/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.example.e_commerce_backend.Product;
+
+public interface ProductRepository {
+}
