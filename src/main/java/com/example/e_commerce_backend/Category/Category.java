3ca016454fc18174d9fe8b11762b8e197397426c
AlexNdungu <alexmeta517@gmail.com>
2024-12-14 12:54:08 +0300

    create columns

--- src/main/java/com/example/e_commerce_backend/Category/Category.java
+++ src/main/java/com/example/e_commerce_backend/Category/Category.java
@@ -1,5 +1,6 @@
 package com.example.e_commerce_backend.Category;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,17 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name")
     private String name;
+
+    @Column(name = "category_description")
     private String description;
 }
--- src/main/java/com/example/e_commerce_backend/Product/Product.java
+++ src/main/java/com/example/e_commerce_backend/Product/Product.java
@@ -1,5 +1,6 @@
 package com.example.e_commerce_backend.Product;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,24 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
     private String name;
+
+    @Column(name = "product_description")
     private String description;
+
+    @Column(name = "product_price")
     private double price;
+
+    @Column(name = "product_image")
     private byte[] image;
 
 }
